58084d368dda21f595bdb6ac9ecc38f95bd8ccea
fares <fares12yaseer45@gmail.com>
2023-02-13 17:50:24 +0200

    start create third section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import MainSection from './Components/MainSection/MainSection'
 import SecondSection from './Components/SecondSection/SecondSection'
+import ThiredSection from './Components/ThiredSection/ThiredSection'
 
 const App = () => {
   return (
     <div className='app'>
       <MainSection />
       <SecondSection />
+      <ThiredSection />
     </div>
   )
 }
---
+++ src/Components/ThiredSection/ThiredSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ThiredSection.scss'
+
+const ThiredSection = () => {
+    return (
+        <div className='third-section'>
+            <div className="container">
+                <div className="Box CBox">
+                    <div className="title"></div>
+                    <div className="Desc"></div>
+                    <div className="Extentions"></div>
+                    <div className="SubscribButton"></div>
+                </div>
+                <div className="Box IBox"></div>
+            </div>
+        </div>
+    )
+}
+
+export default ThiredSection
