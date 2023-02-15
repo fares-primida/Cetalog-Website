417a2f0b621608cf8098ce1e60e214c5af65d115
fares <fares12yaseer45@gmail.com>
2023-02-15 18:34:31 +0200

    finsih the special function

---
+++ src/Components/SecondSection/Data.js
@@ -0,0 +1,33 @@
+import Seo from './seo.png' 
+import Social from './socila-marketing.png'
+import Compititor from './competitor.png' 
+
+export const data =[
+    {
+        BoxclassName: "Box1",
+        ImgClassName : 'imageDiv',
+        ImgContainer : 'imageContainer',
+        ImgSrc : Seo,
+        Others : "Others",
+        H2Text : 'SEO Consultancy',
+        PText : 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly.'
+    } ,
+    {
+        BoxclassName: "Box2",
+        ImgClassName : 'imageDiv',
+        ImgContainer : 'imageContainer',
+        ImgSrc : Compititor,
+        Others : "Others",
+        H2Text : 'Competitor Analysis',
+        PText : 'His defective nor convinced residence own. Connection has put impossible own apartments boisterous.'
+    } ,
+    {
+        BoxclassName: "Box3",
+        ImgClassName : 'imageDiv',
+        ImgContainer : 'imageContainer',
+        ImgSrc : Social,
+        Others : "Others",
+        H2Text : 'Social Marketing',
+        PText : 'John he give of From they fine rich he. They age and draw mrs like. Improving end distrusts.'
+    }
+]
--- src/Components/SecondSection/SecondSection.jsx
+++ src/Components/SecondSection/SecondSection.jsx
@@ -1,67 +1,48 @@
 import React from 'react'
 import './SecondSection.scss'
-import Seo from './seo.png' 
-import Social from './socila-marketing.png'
-import Compititor from './competitor.png' 
+import {data} from './Data.js'
 import { createElement } from 'react'
 
-const data =[
-    {
-        BoxclassName: "Box1",
-        ImgClassName : 'imageDiv',
-        ImgContainer : 'imageContainer',
-        ImgSrc : Seo,
-        Others : "Others",
-        H2Text : 'SEO Consultancy',
-        PText : 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly.'
-    } ,
-    {
-        BoxclassName: "Box2",
-        ImgClassName : 'imageDiv',
-        ImgContainer : 'imageContainer',
-        ImgSrc : Compititor,
-        Others : "Others",
-        H2Text : 'Competitor Analysis',
-        PText : 'His defective nor convinced residence own. Connection has put impossible own apartments boisterous.'
-    } ,
-    {
-        BoxclassName: "Box3",
-        ImgClassName : 'imageDiv',
-        ImgContainer : 'imageContainer',
-        ImgSrc : Social,
-        Others : "Others",
-        H2Text : 'Social Marketing',
-        PText : 'John he give of From they fine rich he. They age and draw mrs like. Improving end distrusts.'
-    }
-]
 
 
-function CreateElements()  {
-        for(let i = 0; i < data.length; i++) {
-            return React.createElement('div' , {className : data[i].BoxclassName},
-            createElement('div' , {className : data[i].ImgClassName}, 
-            createElement('div' , {className : data[i].ImgContainer}, 
-            createElement('img' , {src : data[i].ImgSrc})),
+
+function CreateElements({BCN , ICN , IC , IS , O , H2 , P})  {
+            return React.createElement('div' , {className : BCN},
+            createElement('div' , {className : ICN}, 
+            createElement('div' , {className : IC}, 
+            createElement('img' , {src : IS})),
             ),
-            createElement('div' , {className : data[i].Others},
-            createElement('h2' , null , data[i].H2Text),
-            createElement('p' , null , data[i].PText)
+            createElement('div' , {className : O},
+            createElement('h2' , null , H2),
+            createElement('p' , null , P)
             )
             )
-        }
+    }
 
+    const Category = () => {
+        for(let i = 0; i < data.length; i++) {
+            yy.push(<CreateElements 
+                key={i}
+                BCN={data[i].BoxclassName} 
+                ICN={data[i].ImgClassName} 
+                IC={data[i].ImgContainer}
+                IS={data[i].ImgSrc}
+                O={data[i].Others}
+                H2={data[i].H2Text}
+                P={data[i].PText}
+                />) 
+        }
+        return yy
     }
+
     const yy = []
-    for(let i = 0; i < data.length; i++) {
-                yy.push(<CreateElements key={i}/>)
-    }
 
 const SecondSection = () => {
+    console.log(yy)
     return (
         <div className='second-section'>
             <div className="container">
-                {/* {<CreateElements key={2}/>} */}
-                {yy}
+                <Category />
             </div>
         </div>
     )
